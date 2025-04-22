7ee8c27c8373146863bb79ea032857734ac476f7
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-22 11:07:11 +0300

    learned  how slicing works

---
+++ strings.py
@@ -0,0 +1,8 @@
+str = "DoctorPhenomenal"
+
+# str[::2] meeans slicing
+# [::2] means, start from the beginnging , and take every second character
+print(str[::2]) # prints DcoPeoea
+
+str = "Geeks"
+print(str[::2]) 
