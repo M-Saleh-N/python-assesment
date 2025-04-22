a1100af2357b21cdde423fe3920bc9cb3e3bd1e8
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-22 11:40:00 +0300

    tried questions

--- strings2.py
+++ strings2.py
@@ -7,3 +7,20 @@ x = 3
 for i in range(x, -1, -1):
     print(i, end = ' ' ) # Normally, print() adds a new line after each print.
                          # But with end=' ', you're telling it: “Instead of a new line, just put a space.”
+print(" ")
+# Try it:
+# Use s = 5
+# Use t = 10
+print("---------------")
+
+s = 5 
+for i in range(s, -1, -1):
+    print(i, end = ' ')
+
+print(" ")
+
+print("---------------")
+
+t = 10 
+for i in range(t, -1, -1):
+    print(i, end = ' ')
