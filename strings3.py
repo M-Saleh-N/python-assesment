8ddd17a9bfe9509f21ac91954e18757311be64bb
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-22 12:11:52 +0300

    tried questions on it

--- strings3.py
+++ strings3.py
@@ -13,3 +13,41 @@ elif n > 0:
     pos(n)
 else:
     neg(n)
+
+# try
+# n = -5
+# n = 0
+# n = 2
+
+def pos(n):
+    for i in range(n-1, -1, -1):
+        print(i, end=' ')
+
+def neg(n):
+    for i in range(n, 1):
+        print(i, end=' ')
+
+n = -5
+if n == 0:
+    print("already Zero")
+elif n > 0:
+    pos(n)
+else:
+    neg(n)
+
+
+def pos(n):
+    for i in range(n-1, -1, -1):
+        print(i, end=' ')
+
+def neg(n):
+    for i in range(n, 1):
+        print(i, end=' ')
+
+n = 2
+if n == 0:
+    print("already Zero")
+elif n > 0:
+    pos(n)
+else:
+    neg(n)
